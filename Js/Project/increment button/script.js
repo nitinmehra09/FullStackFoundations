47d3c63e6312ec2e0ad2e7a77508468793db729f
nitinmehra09 <nitinmehra9983@gmail.com>
2026-02-14 20:02:12 +0530

    now this project is completed

--- Js/Project/increment button/script.js
+++ Js/Project/increment button/script.js
@@ -1,13 +1,35 @@
 let count = 0
-let previousEntires = document.getElementById("save-el");
+let previousEntries = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
+let hasSaved = false;
 
 function increment() {
-    count = count +1;
+    if(count >= 10){
+        alert("Limit Reached 🚫");
+    }else{
+        count = count +1;
+        countEl.innerText = count;
+    }
+}
+function decrement() {
+    if(count>0){
+        count = count -1;
+    }
     countEl.innerText = count;
 }
+function reset() {
+    count = 0;
+    countEl.innerText = count;
+    previousEntries.textContent = "Previous entries: 0";
+    hasSaved = false;
+}
 function save(){
-    previousEntires.textContent += (count + " - ");
+    if(hasSaved){
+        previousEntries.textContent += (count + " - ");
+    }else{
+        previousEntries.textContent = ("Previous entries: " +(count + " - "));
+        hasSaved = true;
+    }
     countEl.innerText = 0;
     count = 0;
     console.log(count);
